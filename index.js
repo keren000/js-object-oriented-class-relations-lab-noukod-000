83dc6af14c8c30a423b59828eb187ed9d0d5345f
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 14:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,67 +1,381 @@
 let store = {drivers: [], passengers: [], trips: []};
 
-let driverId = 0
 
-class Driver{
-  constructor(name){
-    this.id = driverId++
+
+
+
+
+
+
+
+
+
+let driverId = 0;
+
+
+
+
+
+
+
+class Driver {
+
+
+
+  constructor(name,passenger) {
+
+
+
+    this.id = ++driverId;
+
+
+
     this.name = name;
+
+
+
+    if (passenger) {
+
+
+
+      this.passengerId = passenger.id;
+
+
+
+    }
+
+
+
+
+
+
+
     store.drivers.push(this);
- }
 
-  trips(){
-    return store.trips.filter(function(trip){
-      return trip.driverId === this.id;
-    }.bind(this))
-  };
 
-  passengers(){
-    return this.trips().map(function(trip){
-      return trip.passenger();
-    }.bind(this))
-  };
-};
 
-let passengerId = 0
-class Passenger{
-  constructor(name){
-    this.id = passengerId++
+  }
+
+
+
+
+
+
+
+  trips () {
+
+
+
+    return store.trips.filter(
+
+
+
+      function(trip) {
+
+
+
+        return trip.driverId === this.id;
+
+
+
+      }.bind(this));
+
+
+
+    }
+
+
+
+
+
+
+
+  passengers() {
+
+
+
+      return this.trips().map(trip => {
+
+
+
+        return trip.passenger();
+
+
+
+    });
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+let passengerId = 0;
+
+
+
+
+
+
+
+class Passenger {
+
+
+
+  constructor(name, driver) {
+
+
+
+    this.id = ++passengerId;
+
+
+
     this.name = name;
+
+
+
+        if (driver) {
+
+
+
+      this.driverId = driver.id;
+
+
+
+    }
+
+
+
+
+
+
+
     store.passengers.push(this);
-  }
 
-  trips(){
-    return store.trips.filter(function(trip){
-      return trip.passengerId === this.id;
-    }.bind(this))
-  }
 
-  drivers(){
-    return this.trips().map(function(trip){
-      return trip.driver();
-    }.bind(this))
-  }
-};
 
-let tripId = 0
-class Trip{
-  constructor(driver, passenger){
-    this.driverId = driver.id;
-    this.passengerId = passenger.id;
-    this.id = tripId++
-    store.trips.push(this);
-  }
 
-  driver(){
-    return store.drivers.find(function(driver){
-      return driver.id === this.driverId;
-    }.bind(this))
-  }
 
-  passenger(){
-    return store.passengers.find(function(passenger){
-      return passenger.id === this.passengerId;
-    }.bind(this))
+
 
   }
-};
+
+
+
+
+
+
+
+  trips () {
+
+
+
+    return store.trips.filter(
+
+
+
+      function(trip) {
+
+
+
+        return trip.passengerId === this.id;
+
+
+
+      }.bind(this));
+
+
+
+  }
+
+
+
+
+
+
+
+  drivers () {
+
+
+
+    // return store.drivers.filter(
+
+
+
+    //   function(driver) {
+
+
+
+    //     return driver.passengerId === this.id;
+
+
+
+    //   }.bind(this));
+
+
+
+      return this.trips().map(trip => {
+
+
+
+        return trip.driver();
+
+
+
+    });
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+let tripId = 0;
+
+
+
+
+
+
+
+class Trip {
+
+
+
+
+
+
+
+  constructor (driver, passenger){
+
+
+
+        this.id = ++tripId;
+
+
+
+        this.driverId = driver.id;
+
+
+
+        this.passengerId = passenger.id;
+
+
+
+
+
+
+
+        store.trips.push(this);
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+  driver () {
+
+
+
+    return store.drivers.find(
+
+
+
+      function(driver) {
+
+
+
+        return driver.id === this.driverId;
+
+
+
+      }.bind(this)
+
+
+
+      );
+
+
+
+  }
+
+
+
+
+
+
+
+  passenger () {
+
+
+
+    return store.passengers.find(
+
+
+
+      function(passenger){
+
+
+
+        return passenger.id === this.passengerId;
+
+
+
+      }.bind(this)
+
+
+
+      );
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
